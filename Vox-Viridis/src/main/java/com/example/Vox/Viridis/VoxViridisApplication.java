2d76aec9b8d3d6b28f93ba9fa8a0d9b6ac062fb4
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-09-18 01:04:54 +0800

    Initial Commit

---
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/VoxViridisApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vox.Viridis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxViridisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoxViridisApplication.class, args);
+	}
+
+}

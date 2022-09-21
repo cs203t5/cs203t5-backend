cf2ffc2681ec16037824dcd7159aaac5b711d3b5
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-09-21 18:38:41 +0800

    trigger github action

---
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/Controller/VoxViridisController.java
@@ -0,0 +1,29 @@
+package com.example.Vox.Viridis.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class VoxViridisController {
+
+    public VoxViridisController() {
+    }
+
+    @GetMapping("/books")
+    public List<String> getBooks() {
+        // System.out.println("okkk");
+        List<String> list = new ArrayList<>();
+        list.add("okk");
+        list.add("bleh bleh");
+        list.add("bleh bleh");
+        list.add("frankeee abang");
+        return list;
+    }
+
+}

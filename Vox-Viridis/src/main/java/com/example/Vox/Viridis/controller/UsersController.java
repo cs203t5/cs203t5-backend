89b9d2823fc1cc49d68b09712cfbebd2bb74fd63
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2022-09-29 02:10:17 +0800

    some auth stff

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/controller/UsersController.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.example.Vox.Viridis.controller;
 import java.net.URI;
 import java.security.Principal;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.Vox.Viridis.model.Users;
 import com.example.Vox.Viridis.model.dto.UsersDTO;
+import com.example.Vox.Viridis.service.TokenService;
 import com.example.Vox.Viridis.service.UsersService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController()
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UsersController {
     private final UsersService usersService;
+    private final TokenService tokenService;
 
     @GetMapping("/name")
     public String getName(Principal principal) {
@@ -30,5 +35,13 @@ public class UsersController {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/api/user/save").toUriString());
         return ResponseEntity.created(uri).body(usersService.saveUser(user));
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        log.debug("Token requested for user: '{}'", authentication.getName());
+        String token = tokenService.generateToken(authentication);
+        log.debug("Token granted {}", token);
+        return token;
     }
 }

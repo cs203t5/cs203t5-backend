fc4813064d5cd8a62d3aff884e09d4e79a6829fe
ernst12 <50575073+ernst12@users.noreply.github.com>
2022-10-02 02:11:36 +0800

    Fix validation issue with Campaign modal

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/model/Campaign.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/model/Campaign.java
@@ -1,6 +1,7 @@
 package com.example.Vox.Viridis.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,11 +9,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Data
@@ -31,11 +37,24 @@ public class Campaign {
 
     private String description;
 
+    @NotNull(message = "Campaign's startDateshould not be null")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate startDate;
+
     @NotNull(message = "Campaign's startTime should not be null")
-    private LocalDateTime startTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "HH:mm")
+    private Date startTime;
+
+    @NotNull(message = "Campaign's endDate should not be null")
+    // @JsonFormat(pattern = "dd-MM-yyyy")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate endDate;
 
     @NotNull(message = "Campaign's endTime should not be null")
-    private LocalDateTime endTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "HH:mm")
+    private Date endTime;
     
     private String location;
     private char status;

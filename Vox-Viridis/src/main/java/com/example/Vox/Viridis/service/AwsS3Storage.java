188fa2fe54bc233d45743c734dd5e2d05ada250d
ernst12 <50575073+ernst12@users.noreply.github.com>
2022-10-02 02:19:35 +0800

    Remove redundant import

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/service/AwsS3Storage.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/service/AwsS3Storage.java
@@ -13,7 +13,6 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.Vox.Viridis.config.AWSConfig;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service

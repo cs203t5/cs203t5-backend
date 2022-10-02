05a52fd093c14cab11d30f86611502fce95e054b
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2022-10-02 15:26:43 +0800

    Update UsersService.java

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/service/UsersService.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/service/UsersService.java
@@ -1,16 +1,8 @@
 package com.example.Vox.Viridis.service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Optional;
 import javax.transaction.Transactional;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import com.example.Vox.Viridis.model.SecurityUser;
 import com.example.Vox.Viridis.model.Users;
 import com.example.Vox.Viridis.model.dto.UsersDTO;
 import com.example.Vox.Viridis.repository.UsersRepository;
@@ -24,22 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 public class UsersService {
     private final UsersRepository usersRepository;
     private final PasswordEncoder passwordEncoder;
-
-    // @Override
-    // public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // Optional<Users> user = usersRepository.findByUsername(username);
-
-    // if (user == null) {
-    // log.error("User not found: " + username);
-    // throw new UsernameNotFoundException("User not found");
-    // } else {
-    // log.info("User found: " + username);
-    // }
-    // Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-    // authorities.add(new SimpleGrantedAuthority("user"));
-    // return new org.springframework.security.core.userdetails.User(user.getUsername(),
-    // user.getPassword(), authorities);
-    // }
 
     public UsersDTO saveUser(Users user) {
         log.info("Saving new user to the database");

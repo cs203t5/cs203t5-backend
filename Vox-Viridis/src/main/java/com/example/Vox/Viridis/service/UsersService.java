28ab8b13e82e1ea8316ce18e865e85eeec027c87
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2022-10-02 20:45:40 +0800

    split role and users

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/service/UsersService.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/service/UsersService.java
@@ -1,10 +1,14 @@
 package com.example.Vox.Viridis.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import com.example.Vox.Viridis.model.Role;
 import com.example.Vox.Viridis.model.Users;
 import com.example.Vox.Viridis.model.dto.UsersDTO;
+import com.example.Vox.Viridis.repository.RoleRepository;
 import com.example.Vox.Viridis.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +20,15 @@ import lombok.extern.slf4j.Slf4j;
 public class UsersService {
     private final UsersRepository usersRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     public UsersDTO saveUser(Users user) {
         log.info("Saving new user to the database");
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        List<Role> roles = new ArrayList<>();
+        Role consumer = roleRepository.findByName("CONSUMER");
+        roles.add(consumer);
+        user.setRoles(roles);
         Users saveUser = usersRepository.save(user);
         UsersDTO usersDTO = new UsersDTO();
         usersDTO.setUsername(saveUser.getUsername());

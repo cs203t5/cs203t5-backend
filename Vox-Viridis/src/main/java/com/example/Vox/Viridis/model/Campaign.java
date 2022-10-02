ec19b21ec07fd5954a1c08db748912a58d4782b7
ernst12 <50575073+ernst12@users.noreply.github.com>
2022-10-02 17:36:29 +0800

    remove redundant import

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/model/Campaign.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/model/Campaign.java
@@ -13,7 +13,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;

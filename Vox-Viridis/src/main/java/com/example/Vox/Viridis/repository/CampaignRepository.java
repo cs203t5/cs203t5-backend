a35072ac7c110f00dff72973d7b8c193978720a8
ernst12 <50575073+ernst12@users.noreply.github.com>
2022-10-14 23:43:29 +0800

    Bug fix: GET campaign API return duplicated record

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/repository/CampaignRepository.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/repository/CampaignRepository.java
@@ -12,7 +12,7 @@ import com.example.Vox.Viridis.model.Campaign;
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
     List<Campaign> findByTitle(String title);
 
-    @Query(value = "SELECT c FROM Campaign c JOIN c.rewards r JOIN r.rewardType rt WHERE c.title LIKE %:title% AND (COALESCE(:category) IS NULL OR c.category IN :category) AND (COALESCE(:location) IS NULL OR c.location IN :location) AND (COALESCE(:reward) IS NULL OR rt.rewardType IN :reward) AND c.endDate >= NOW()")
+    @Query(value = "SELECT c FROM Campaign c WHERE c.title LIKE %:title% AND (COALESCE(:category) IS NULL OR c.category IN :category) AND (COALESCE(:location) IS NULL OR c.location IN :location) AND (COALESCE(:reward) IS NULL OR (SELECT COUNT(rt) FROM c.rewards r JOIN r.rewardType rt WHERE rt.rewardType IN :reward)>0) AND c.endDate >= NOW()")
     Page<Campaign> findByTitleAndCategoryAndLocationAndReward(String title, List<String> category, List<String> location, List<String> reward, Pageable pageable);
 
     @Query(nativeQuery = true, value = "SELECT created_by FROM campaign WHERE id = :id")

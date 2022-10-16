39f3b0a242fc2bf1183f609e6c2ca74f268f8fd5
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2022-10-16 21:46:28 +0800

    send email

---
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/controller/EmailController.java
@@ -0,0 +1,37 @@
+package com.example.Vox.Viridis.controller;
+
+import javax.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.Vox.Viridis.model.Contact;
+import com.example.Vox.Viridis.service.MailService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("email")
+@RequiredArgsConstructor
+public class EmailController {
+    private final MailService mailService;
+
+    @PostMapping
+    public ResponseEntity<Object> sendEmail(@Valid @RequestBody Contact email) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("voxviridis5@gmail.com");
+        simpleMailMessage.setTo("voxviridis5@gmail.com");
+        simpleMailMessage
+                .setSubject("Question from " + email.getFirstName() + " " + email.getLastName());
+        simpleMailMessage.setText("Email: " + email.getEmail() + "\n" + "First Name: "
+                + email.getFirstName() + "\n" + "Last name: " + email.getLastName() + "\n"
+                + "Question: " + email.getQuestion() + "\n");
+        boolean isSent = mailService.sendMailMessage(simpleMailMessage);
+        if (isSent) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}

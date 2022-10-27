7e97e1a38042f0ae5c008e777b0e4001d549c96f
Valentino Ong <valentinoo.2021@scis.smu.edu.sg>
2022-10-28 02:32:33 +0800

    stuff added

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/repository/ProductsRepository.java
+++ Vox-Viridis/src/main/java/com/example/Vox/Viridis/repository/ProductsRepository.java
@@ -1,11 +1,11 @@
 package com.example.Vox.Viridis.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.Vox.Viridis.model.Products;
 
 public interface ProductsRepository extends JpaRepository<Products,Long>{
-    Optional<Products> findByName(String name);
+    List<Products> findByName(String name);
 }

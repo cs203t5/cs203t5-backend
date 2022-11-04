d9687a861c2f776aec8476a49b47640ba4cac4ff
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2022-11-04 16:24:32 +0800

    adding repackage

--- Vox-Viridis/src/main/java/com/example/Vox/Viridis/Controller/VoxViridisController.java
+++
@@ -1,9 +0,0 @@
-package com.example.Vox.Viridis.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class VoxViridisController {
-
-    public VoxViridisController() {}
-}
